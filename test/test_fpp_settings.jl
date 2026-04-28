895f63f67d8a19ccd9e0f9ca7d80a4dda7ad8b66
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-28 15:22:23 +0200

    Refactoring

--- test/test_fpp_settings.jl
+++ test/test_fpp_settings.jl
@@ -1,9 +1,9 @@
 # activate the test environment if needed
-if !@isdefined(KiteControllers)
-    import Pkg
+using Pkg
+if ! ("Test" ∈ keys(Pkg.project().dependencies))
     Pkg.activate(@__DIR__)
-    using Test, KiteControllers
 end
+using Test, KiteControllers
 
 @testset "FPPSettings defaults" begin
     fpps = FPPSettings()
