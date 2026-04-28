8ae5c01e1b9a986461092f33fcd5613d960759a9
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-04-28 15:18:21 +0200

    Refactoring

--- test/test_flightpathcontroller.jl
+++ test/test_flightpathcontroller.jl
@@ -1,9 +1,9 @@
 # activate the test environment if needed
-if !@isdefined(KiteControllers)
-    import Pkg
+using Pkg
+if ! ("Test" ∈ keys(Pkg.project().dependencies))
     Pkg.activate(@__DIR__)
-    using Test, KiteControllers
 end
+using Test, KiteControllers, KiteModels
 
 @testset "FlightPathController" begin
     fcs = FPCSettings(dt=0.05)
