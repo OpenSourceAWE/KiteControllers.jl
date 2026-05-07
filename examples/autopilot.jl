9c6f50698c8bc269aa327c5040aea5d58c5941ba
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-05-07 18:22:04 +0200

    Move plot to analyse.jl

--- examples/autopilot.jl
+++ examples/autopilot.jl
@@ -577,25 +577,6 @@ end
 stop_()
 KiteViewers.GLMakie.closeall()
 
-# Plot heading_rate and body_rate from the last recorded log file
-let
-    log_path = joinpath(OUTPUT_DIR, "last_sim_log")
-    if isfile(log_path * ".arrow")
-        log = load_log(basename(log_path); path=dirname(log_path))
-        sl = log.syslog
-        heading_rate_deg = rad2deg.(sl.heading_rate)
-        body_rate_deg = rad2deg.([tr[3] for tr in sl.turn_rates])
-        p = ControlPlots.plot(sl.time, [heading_rate_deg, body_rate_deg];
-                              xlabel="time [s]", ylabel="rate [°/s]",
-                              labels=["heading_rate", "body_rate"],
-                              fig="rates")
-        display(p)
-        println("Plotted heading_rate and body_rate from: $log_path")
-    else
-        println("No log file found at: $(log_path).arrow")
-    end
-end
-
 GC.enable(true)
 nothing
 
